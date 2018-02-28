6cb23d9e9e06f5bfdb04c396b20791209d097f46
hermetico <yojmarinv@gmail.com>
2018-02-28 22:24:42 +0100

    basic clock functionality

--- project-1/src/main/java/bufmgr/Clock.java
+++ project-1/src/main/java/bufmgr/Clock.java
@@ -11,24 +11,25 @@ public class Clock extends Replacer{
 	@Override
 	public void newPage(FrameDesc fdesc) {
 		// TODO Auto-generated method stub
-		
+        //TODO why do we need this?
 	}
 
 	@Override
 	public void freePage(FrameDesc fdesc) {
 		// TODO Auto-generated method stub
-		
+        //TODO why do we need this?
 	}
 
 	@Override
 	public void pinPage(FrameDesc fdesc) {
-
+        fdesc.state = 1;
 		
 	}
 
 	@Override
 	public void unpinPage(FrameDesc fdesc) {
-		// TODO Auto-generated method stub
+        // TODO Auto-generated method stub
+		//TODO why do we need this?
 		
 	}
 
@@ -37,7 +38,20 @@ public class Clock extends Replacer{
 		// TODO Auto-generated method stub
 
 		// when ticking tick = (tick + 1) % frametab.length;
-		return 0;
+        int keep_looping = frametab.length * 2;
+
+        while (keep_looping > 0){
+
+            if(frametab[tick].state == 0){
+                return tick;
+            }else if(frametab[tick].pincnt == 0){
+                frametab[tick].state = 0;
+            }
+
+            tick = (tick + 1) % frametab.length;
+        }
+
+		return -1;
 	}
 
 }
