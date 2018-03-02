a54e324583b05a288946475bfe3d5bbd5defd531
hermetico <yojmarinv@gmail.com>
2018-03-02 14:15:21 +0100

    old comments removed from Clock

--- project-1/src/main/java/bufmgr/Clock.java
+++ project-1/src/main/java/bufmgr/Clock.java
@@ -11,33 +11,26 @@ public class Clock extends Replacer{
 	@Override
 	public void newPage(FrameDesc fdesc) {
 		// TODO Auto-generated method stub
-        //TODO why do we need this?
 	}
 
 	@Override
 	public void freePage(FrameDesc fdesc) {
 		// TODO Auto-generated method stub
-        //TODO why do we need this?
 	}
 
 	@Override
 	public void pinPage(FrameDesc fdesc) {
         fdesc.state = 1;
-		
 	}
 
 	@Override
 	public void unpinPage(FrameDesc fdesc) {
         // TODO Auto-generated method stub
-		//TODO why do we need this?
-		
 	}
 
 	@Override
 	public int pickVictim() {
-		// TODO Auto-generated method stub
-
-		// when ticking tick = (tick + 1) % frametab.length;
+		// worst case scenario
         int keep_looping = frametab.length * 2;
 
         while (keep_looping > 0){
